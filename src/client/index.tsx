6e25b188a8e648e928fe27b5bd03f574774b85cb
ReaganBruce <ReaganBruceBirmingham@gmail.com>
2021-04-08 20:48:26 -0500

    removed html/css imports from react html page

--- src/client/index.tsx
+++ src/client/index.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import { render } from 'react-dom';
 import App from './App';
-import './scss/vendorProfile';
-import './scss/contact';
+import './scss/index.scss'
+
 
 render(<App />, document.getElementById("root"));
